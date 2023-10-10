27112b872e099e171d5393bc11ea39d05b9df04e
vikasraja1 <96073748+vikasraja1@users.noreply.github.com>
2023-10-11 01:38:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,58 @@
+var crsr = document.querySelector("#cursor")
+var blur = document.querySelector("#cursor-blur")
+
+
+document.addEventListener("mousemove",function(dets){
+    crsr.style.left = dets.x+"px"
+    crsr.style.top = dets.y+"px"
+    blur.style.left=dets.x -150 + "px"
+    blur.style.top = dets.y - 150 + "px"
+})
+
+
+
+gsap.to("#nav",{
+
+  backgroundColor: "#000" ,
+  height:"110px",
+  duration:0.5,
+  scrollTrigger:{
+    trigger:"#nav",
+    scroller:"body",
+   // markers:true,
+    start:"top -10%",
+    end:"top -11%",
+    scrub:1
+    
+  }
+
+})
+
+gsap.to("#main" ,{ 
+
+    backgroundColor:"#000",
+    scrollTrigger:{
+        trigger:"#main",
+        scroller:"body",
+        markers:true,
+        start:"top -25%",
+        end:"top -70%",
+        scrub:2
+    }
+
+})
+
+
+var h4all = document.querySelectorAll("#nav h4")
+h4all.forEach(function(elem){
+    elem.addEventListener("mouseenter",function(){
+        crsr.style.scale = 3
+        crsr.style.border = "1px solid #fff"
+        crsr.style.backgroundColor = "transparent"
+    })
+    elem.addEventListener("mouseleave",function(){
+        crsr.style.scale = 1
+        crsr.style.border = "0px solid #95C11E"
+        crsr.style.backgroundColor = "#95C11E"
+    })
+})
